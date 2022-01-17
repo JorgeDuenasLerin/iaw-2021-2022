a7943f312cf03f82efa3e0bccf1b9015fcf00fae
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-01-17 12:03:01 +0100

    Ejemplo de HTML CSS y PHP

---
+++ 01-Introducción/index.php
@@ -0,0 +1,58 @@
+<?php
+$miprimeravariable=5;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página de introducción</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div id="cabecera">
+        <h1>Esto es mi cabecera</h1>
+        <?php 
+            echo $miprimeravariable;
+        ?>
+    </div>
+    <div id="contenido">
+        <div class="noticia">
+            <h1>Título</h1>
+            <img src="https://picsum.photos/100/150" alt="">
+            Lorem fistrum me cago en tus muelas diodenoo llevame al sircoo a wan diodeno al ataquerl pecador fistro papaar papaar está la cosa muy malar. A gramenawer no puedor te voy a borrar el cerito caballo blanco caballo negroorl. Qué dise usteer ahorarr quietooor por la gloria de mi madre la caidita. No puedor me cago en tus muelas a peich a gramenawer hasta luego Lucas al ataquerl. Diodenoo torpedo tiene musho peligro de la pradera apetecan pupita se calle ustée qué dise usteer.
+        </div>
+    </div>
+    <div id="contenido">
+        <div class="noticia">
+            <h1>Título</h1>
+            <img src="https://picsum.photos/100/150" alt="">
+            Lorem fistrum me cago en tus muelas diodenoo llevame al sircoo a wan diodeno al ataquerl pecador fistro papaar papaar está la cosa muy malar. A gramenawer no puedor te voy a borrar el cerito caballo blanco caballo negroorl. Qué dise usteer ahorarr quietooor por la gloria de mi madre la caidita. No puedor me cago en tus muelas a peich a gramenawer hasta luego Lucas al ataquerl. Diodenoo torpedo tiene musho peligro de la pradera apetecan pupita se calle ustée qué dise usteer.
+        </div>
+        <div class="noticia">
+            <h1>Título</h1>
+            <img src="https://picsum.photos/100/150" alt="">
+            Lorem fistrum me cago en tus muelas diodenoo llevame al sircoo a wan diodeno al ataquerl pecador fistro papaar papaar está la cosa muy malar. A gramenawer no puedor te voy a borrar el cerito caballo blanco caballo negroorl. Qué dise usteer ahorarr quietooor por la gloria de mi madre la caidita. No puedor me cago en tus muelas a peich a gramenawer hasta luego Lucas al ataquerl. Diodenoo torpedo tiene musho peligro de la pradera apetecan pupita se calle ustée qué dise usteer.
+        </div>
+        <div class="noticia">
+            <h1>Título</h1>
+            <img src="https://picsum.photos/100/150" alt="">
+            Lorem fistrum me cago en tus muelas diodenoo llevame al sircoo a wan diodeno al ataquerl pecador fistro papaar papaar está la cosa muy malar. A gramenawer no puedor te voy a borrar el cerito caballo blanco caballo negroorl. Qué dise usteer ahorarr quietooor por la gloria de mi madre la caidita. No puedor me cago en tus muelas a peich a gramenawer hasta luego Lucas al ataquerl. Diodenoo torpedo tiene musho peligro de la pradera apetecan pupita se calle ustée qué dise usteer.
+        </div>
+        <div class="noticia">
+            <h1>Título</h1>
+            <img src="https://picsum.photos/100/150" alt="">
+            Lorem fistrum me cago en tus muelas diodenoo llevame al sircoo a wan diodeno al ataquerl pecador fistro papaar papaar está la cosa muy malar. A gramenawer no puedor te voy a borrar el cerito caballo blanco caballo negroorl. Qué dise usteer ahorarr quietooor por la gloria de mi madre la caidita. No puedor me cago en tus muelas a peich a gramenawer hasta luego Lucas al ataquerl. Diodenoo torpedo tiene musho peligro de la pradera apetecan pupita se calle ustée qué dise usteer.
+        </div>
+        <div class="noticia">
+            <h1>Título</h1>
+            <img src="https://picsum.photos/100/150" alt="">
+            Lorem fistrum me cago en tus muelas diodenoo llevame al sircoo a wan diodeno al ataquerl pecador fistro papaar papaar está la cosa muy malar. A gramenawer no puedor te voy a borrar el cerito caballo blanco caballo negroorl. Qué dise usteer ahorarr quietooor por la gloria de mi madre la caidita. No puedor me cago en tus muelas a peich a gramenawer hasta luego Lucas al ataquerl. Diodenoo torpedo tiene musho peligro de la pradera apetecan pupita se calle ustée qué dise usteer.
+        </div>
+    </div>
+    <footer>
+        cosas del pie de página
+    </footer>
+</body>
+</html>
