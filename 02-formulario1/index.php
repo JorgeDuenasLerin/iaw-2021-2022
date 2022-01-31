be79f308c13bf61f7ee60fbcaa9dbd65dc0bf265
Jorge Dueñas Lerín <jorge.duenas.lerin@gmail.com>
2022-01-31 11:43:54 +0100

    Continuación Form

--- 02-formulario1/index.php
+++ 02-formulario1/index.php
@@ -7,10 +7,23 @@ $edad_20_39='';
 $edad_40_59='';
 $edad_60_79='';
 
+$errores = false;
+$error_nombre = false;
+$error_apellido = false;
+$error_edad = false;
+
 if(isset($_POST["enviar"])){
     $bienvenido=false;
-    $nombre=$_POST["nombre"];
-    $apellidos=$_POST["apellidos"];
+
+    $nombre = (isset($_POST["nombre"]))?$_POST["nombre"]:'';
+    if(strlen($nombre)<3){
+        $error_nombre = true;
+    }
+
+    $apellidos = (isset($_POST["apellidos"]))?$_POST["apellidos"]:'';
+    if(strlen($apellidos)<3){
+        $error_apellidos = true;
+    }
 
     if(isset($_POST["edad"])){
         /*
@@ -31,12 +44,17 @@ if(isset($_POST["enviar"])){
         */
         ${"edad_".$_POST["edad"]}='checked';
     }
+
+    if($error_nombre || $error_apellidos) {
+        $errores = true;
+    }
 }
 
+/*
 echo "<br>POST:<br>";
 print_r($_POST);
 
-/*
+
 echo "<br>";
 
 echo "GET:<br>";
@@ -60,19 +78,26 @@ print_r($_SERVER);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
     <?php if($bienvenido) { ?>
         <h1>Bienvenido!!!</h1>
     <?php } ?>
     <div>
+        <?php if($errores) { ?>
+            <div class="error">
+                Los datos introducidos tienen errores
+            </div>
+        <?php } ?>
+
         <form action="index.php" method="post">
             <fieldset>
                 <legend>Información Personal</legend>
-                <label for="nombre">Nombre:</label>
-                <input name="nombre" id="nombre" type="text" tabindex="1" value="<?=$nombre?>" />
-                <label for="apellidos">Apellidos:</label>
-                <input name="apellidos" id="apellidos" type="text" tabindex="2" value="<?=$apellidos?>"/>
+                <label <?=($error_nombre)?'class="error"':'';?> for="nombre">Nombre:</label>
+                <input <?=($error_nombre)?'class="background-error"':'';?> name="nombre" id="nombre" type="text" tabindex="1" value="<?=$nombre?>" />
+                <label <?=($error_apellidos)?'class="error"':'';?> for="apellidos">Apellidos:</label>
+                <input <?=($error_apellidos)?'class="background-error"':'';?> name="apellidos" id="apellidos" type="text" tabindex="2" value="<?=$apellidos?>"/>
             </fieldset>
 
             <fieldset>
